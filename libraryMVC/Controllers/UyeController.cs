1f965f47b9c1a7f00c0254b82f291ecaa8bdace3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-28 15:26:26 +0300

    refactoring uyeler

--- libraryMVC/Controllers/UyeController.cs
+++ libraryMVC/Controllers/UyeController.cs
@@ -1,5 +1,9 @@
+using System.Linq;
+using System.Threading.Tasks;
+using libraryMVC.Models;
 using libraryMVC_.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace libraryMVC.Controllers
 {
@@ -10,6 +14,80 @@ namespace libraryMVC.Controllers
         public UyeController(AppDbContext context)
         {
             _context = context;
+        }
+        public IActionResult Uyeler(string searchString, int id)
+        {
+            return View(_context.Uyeler);
+        }
+        [HttpPost]
+        public IActionResult UyelerSearch(string searchString, int id)
+        {
+            if (searchString == null) searchString = "^";
+            searchString = searchString.ToLower();
+            //searchString = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(searchString);
+            var arama = _context.Uyeler.Where(x => x.UyeAd.ToLower().Contains(searchString) || x.UyeSoyad.ToLower().Contains(searchString) || x.UyeEposta.ToLower().Contains(searchString) || x.UyeTelefon.Contains(searchString) || x.UyeAdres.ToLower().Contains(searchString) || x.UyeNo == id).ToList();
+            return View(arama);
+        }
+        public async Task<IActionResult> DeleteUyeler(int id)
+        {
+            var uye = _context.Uyeler.SingleOrDefault(x => x.UyeNo == id);
+            _context.Uyeler.Remove(uye);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Uyeler");
+        }
+        public IActionResult CreateUyeler()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateUyeler(Uye uye)
+        {
+            if (uye.UyeAd.Contains("^") == true) uye.UyeAd = "-";
+            if (uye.UyeSoyad.Contains("^") == true) uye.UyeSoyad = "-";
+            if (uye.UyeTelefon.Contains("^") == true) uye.UyeTelefon = "000 000 00 00";
+            if (uye.UyeEposta.Contains("^") == true) uye.UyeEposta = "unknown@hotmail.com";
+            if (uye.UyeAdres.Contains("^") == true) uye.UyeAdres = "Ankara";
+            _context.Uyeler.Add(uye);
+            _context.SaveChanges();
+            return RedirectToAction("Uyeler");
+        }
+        public IActionResult EditUyeler(int? id)
+        {
+
+            var uye = _context.Uyeler.Where(x => x.UyeNo == id).SingleOrDefault();
+            if (uye == null)
+            {
+
+            }
+            return View(uye);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditUyeler(int id, [Bind("UyeNo,UyeAd,UyeSoyad,UyeTelefon,UyeEposta,UyeAdres")] Uye uye)
+        {
+            if (id != uye.UyeNo)
+            {
+
+            }
+            if (uye.UyeAd.Contains("^") == true) uye.UyeAd = "-";
+            if (uye.UyeSoyad.Contains("^") == true) uye.UyeSoyad = "-";
+            if (uye.UyeEposta.Contains("^") == true) uye.UyeEposta = "unknown@hotmail.com";
+            if (uye.UyeTelefon.Contains("^") == true) uye.UyeTelefon = "000 000 00 00";
+            if (uye.UyeAdres.Contains("^") == true) uye.UyeAdres = "Ankara";
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(uye);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+
+                }
+                return RedirectToAction("Uyeler");
+            }
+            return View(uye);
         }
     }
 }
