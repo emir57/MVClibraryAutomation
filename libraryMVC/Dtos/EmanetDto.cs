6ce375ea735e4db4bfa15df354dae20e0faeaf99
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-29 15:55:52 +0300

    Add Emanet Dto

---
+++ libraryMVC/Dtos/EmanetDto.cs
@@ -0,0 +1,14 @@
+namespace libraryMVC.Dtos
+{
+    public class EmanetDto
+    {
+        public int EmanetNo {get; set;}
+        public string Uye {get; set;}
+        public string Kitap {get; set;}
+        public string EmanetVermeTarih {get; set;}
+        public string EmanetGeriAlmaTarih {get; set;}
+        public string EmanetIslemTarih {get; set;}
+        public string EmanetNot {get; set;}
+        public string EmanetTeslimEdildi {get; set;}
+    }
+}
