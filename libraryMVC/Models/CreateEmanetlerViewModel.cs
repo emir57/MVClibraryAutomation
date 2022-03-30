f258b17c75c781caa4cb501fff0e294e4a9f6f4b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-30 20:18:25 +0300

    CreateEmanetlerViewModel Add Kitaplar

--- libraryMVC/Models/CreateEmanetlerViewModel.cs
+++ libraryMVC/Models/CreateEmanetlerViewModel.cs
@@ -5,6 +5,7 @@ namespace libraryMVC.Models
     public class CreateEmanetlerViewModel
     {
         public List<Uye> Uyeler { get; set; }
+        public List<Kitap> Kitaplar { get; set; }
         public Emanet Emanet { get; set; }
     }
 }
