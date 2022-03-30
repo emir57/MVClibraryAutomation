98723c748962c14c185e732eed924066a078a83f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-30 20:31:48 +0300

    Emanet Add Validation

--- libraryMVC/Models/Emanet.cs
+++ libraryMVC/Models/Emanet.cs
@@ -5,12 +5,17 @@ namespace libraryMVC.Models
     {
         [Key]
         public int EmanetNo {get; set;}
+        [Required(ErrorMessage = "UyeNo boş olamaz")]
         public int UyeNo {get; set;}
+        [Required(ErrorMessage = "KitapNo boş olamaz")]
         public int KitapNo {get; set;}
+        [Required(ErrorMessage = "Emanet verme tarihi boş olamaz")]
         public string EmanetVermeTarih {get; set;}
+        [Required(ErrorMessage = "Emanet geri alma tarihi boş olamaz")]
         public string EmanetGeriAlmaTarih {get; set;}
         public string EmanetIslemTarih {get; set;}
         public string EmanetNot {get; set;}
+        [Required(ErrorMessage = "Emanet teslim edildi boş olamaz")]
         public string EmanetTeslimEdildi {get; set;}
     }
 }
