006c43a8090c117a27b88aeb1bc61c69756b7fb4
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-30 20:16:23 +0300

    CreateEmanetlerViewModel Add Emanet

--- libraryMVC/Models/CreateEmanetlerViewModel.cs
+++ libraryMVC/Models/CreateEmanetlerViewModel.cs
@@ -4,6 +4,7 @@ namespace libraryMVC.Models
 {
     public class CreateEmanetlerViewModel
     {
-        public List<Uye> Uyeler { get; set; } 
+        public List<Uye> Uyeler { get; set; }
+        public Emanet Emanet { get; set; }
     }
 }
