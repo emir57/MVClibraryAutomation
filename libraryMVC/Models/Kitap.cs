37c819758be3c1f7817e34695e61938c621622a0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-31 19:03:41 +0300

    Kitap Add Displays

--- libraryMVC/Models/Kitap.cs
+++ libraryMVC/Models/Kitap.cs
@@ -5,14 +5,28 @@ namespace libraryMVC.Models
     public class Kitap
     {
         [Key]
-        public int KitapNo {get; set;}
-        public string KitapAd {get; set;}
-        public string KitapYazari {get; set;}
-        public int KitapBaskiYil {get; set;}
-        public int KitapSayfaSayi {get; set;}
-        public string KitapDil {get; set;}
-        public string KitapYayinEvi {get; set;}
-        public string KitapAciklama {get; set;}
+        public int KitapNo { get; set; }
+
+        [Display(Name = "Kitap Adı")]
+        public string KitapAd { get; set; }
+
+        [Display(Name = "Kitap Yazarı")]
+        public string KitapYazari { get; set; }
+
+        [Display(Name = "Kitap Baskı Yılı")]
+        public int KitapBaskiYil { get; set; }
+
+        [Display(Name = "Kitap Sayfa Sayısı")]
+        public int KitapSayfaSayi { get; set; }
+
+        [Display(Name = "Kitap Dili")]
+        public string KitapDil { get; set; }
+
+        [Display(Name = "Kitap Yayın Evi")]
+        public string KitapYayinEvi { get; set; }
+
+        [Display(Name = "Kitap Açıklaması")]
+        public string KitapAciklama { get; set; }
 
     }
 }
