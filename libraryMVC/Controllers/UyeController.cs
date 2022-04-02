0cbe88518a9c9612d8f8d202834b93cf69b85899
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-02 15:59:12 +0300

    Success UyeAd

--- libraryMVC/Controllers/UyeController.cs
+++ libraryMVC/Controllers/UyeController.cs
@@ -70,14 +70,9 @@ namespace libraryMVC.Controllers
             {
                 return View(uye);
             }
-            if (uye.UyeAd.Contains("^") == true) uye.UyeAd = "-";
-            if (uye.UyeSoyad.Contains("^") == true) uye.UyeSoyad = "-";
-            if (uye.UyeTelefon.Contains("^") == true) uye.UyeTelefon = "000 000 00 00";
-            if (uye.UyeEposta.Contains("^") == true) uye.UyeEposta = "unknown@hotmail.com";
-            if (uye.UyeAdres.Contains("^") == true) uye.UyeAdres = "Ankara";
             await _context.Uyeler.AddAsync(uye);
             await _context.SaveChangesAsync();
-            return RedirectToAction("Uyeler");
+            return RedirectToAction(nameof(Uyeler), new { @message = $"{uye.UyeAd} {uye.UyeSoyad} başarıyla eklendi" });
         }
         public async Task<IActionResult> EditUyeler(int? id)
         {
