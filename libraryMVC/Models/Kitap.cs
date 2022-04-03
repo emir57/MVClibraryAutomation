255167cc1925514b839d36e6dcfdd0798bb78da8
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-03 15:54:08 +0300

    Refactoring Kitap

--- libraryMVC/Models/Kitap.cs
+++ libraryMVC/Models/Kitap.cs
@@ -8,36 +8,36 @@ namespace libraryMVC.Models
         public int KitapNo { get; set; }
 
         [Display(Name = "Kitap Adı")]
-        [Required(ErrorMessage = "Kitap adı boş olamaz")]
-        [MaxLength(30, ErrorMessage = "Kitap adı maksimum 30 karakter olabilir")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        [MaxLength(30, ErrorMessage = "{0} maksimum 30 karakter olabilir")]
         public string KitapAd { get; set; }
 
         [Display(Name = "Kitap Yazarı")]
-        [Required(ErrorMessage = "Kitap yazarı boş olamaz")]
-        [MaxLength(30, ErrorMessage = "Kitap yazarı maksimum 30 karakter olabilir")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        [MaxLength(30, ErrorMessage = "{0} maksimum 30 karakter olabilir")]
         public string KitapYazari { get; set; }
 
         [Display(Name = "Kitap Baskı Yılı")]
-        [Required(ErrorMessage = "Kitap baskı yılı boş olamaz")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
         public int KitapBaskiYil { get; set; }
 
         [Display(Name = "Kitap Sayfa Sayısı")]
-        [Required(ErrorMessage = "Kitap sayfa sayısı boş olamaz")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
         public int KitapSayfaSayi { get; set; }
 
         [Display(Name = "Kitap Dili")]
-        [Required(ErrorMessage = "Kitap dili boş olamaz")]
-        [MaxLength(20, ErrorMessage = "Kitap dili maksimum 20 karakter olabilir")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        [MaxLength(20, ErrorMessage = "{0} maksimum 20 karakter olabilir")]
         public string KitapDil { get; set; }
 
         [Display(Name = "Kitap Yayın Evi")]
-        [Required(ErrorMessage = "Kitap yayın evi boş olamaz")]
-        [MaxLength(30, ErrorMessage = "Kitap yayın evi maksimum 30 karakter olabilir")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        [MaxLength(30, ErrorMessage = "{0} maksimum 30 karakter olabilir")]
         public string KitapYayinEvi { get; set; }
 
         [Display(Name = "Kitap Açıklaması")]
-        [Required(ErrorMessage = "Kitap açıklaması boş olamaz")]
-        [MaxLength(250, ErrorMessage = "Kitap açıklaması maksimum 250 karakter olabilir")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        [MaxLength(250, ErrorMessage = "{0} maksimum 250 karakter olabilir")]
         public string KitapAciklama { get; set; }
 
     }
