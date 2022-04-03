8413ba174830dcec78b9108ce149203d7042da1d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-03 15:58:05 +0300

    Refactoring Emanet

--- libraryMVC/Models/Emanet.cs
+++ libraryMVC/Models/Emanet.cs
@@ -8,22 +8,19 @@ namespace libraryMVC.Models
 
 
         [Display(Name = "Üye")]
-        [Required(ErrorMessage = "UyeNo boş olamaz")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
         public int UyeNo { get; set; }
 
-
         [Display(Name = "Kitap")]
-        [Required(ErrorMessage = "KitapNo boş olamaz")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
         public int KitapNo { get; set; }
 
-
-        [Required(ErrorMessage = "Emanet verme tarihi boş olamaz")]
         [Display(Name = "Emanet Verme Tarihi")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
         public string EmanetVermeTarih { get; set; }
 
-
-        [Required(ErrorMessage = "Emanet geri alma tarihi boş olamaz")]
         [Display(Name = "Emanet Geri Alma Tarihi")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
         public string EmanetGeriAlmaTarih { get; set; }
 
         [Display(Name = "Emanet İşlem Tarihi")]
@@ -31,9 +28,8 @@ namespace libraryMVC.Models
         [Display(Name = "Emanet Not")]
         public string EmanetNot { get; set; }
 
-
-        [Required(ErrorMessage = "Emanet teslim edildi boş olamaz")]
         [Display(Name = "Emanet Teslim Durumu")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
         public string EmanetTeslimEdildi { get; set; }
     }
 }
