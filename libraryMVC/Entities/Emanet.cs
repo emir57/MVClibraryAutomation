5a0977f249f74547a109de053cf59404de58d9cb
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-04 21:20:57 +0300

    Refactoring Emanet Entity

--- libraryMVC/Entities/Emanet.cs
+++ libraryMVC/Entities/Emanet.cs
@@ -5,30 +5,12 @@ namespace libraryMVC.Entities
     {
         [Key]
         public int EmanetNo { get; set; }
-
-        [Display(Name = "Üye")]
-        [Required(ErrorMessage = "{0} boş olamaz")]
         public int UyeNo { get; set; }
-
-        [Display(Name = "Kitap")]
-        [Required(ErrorMessage = "{0} boş olamaz")]
         public int KitapNo { get; set; }
-
-        [Display(Name = "Emanet Verme Tarihi")]
-        [Required(ErrorMessage = "{0} boş olamaz")]
         public string EmanetVermeTarih { get; set; }
-
-        [Display(Name = "Emanet Geri Alma Tarihi")]
-        [Required(ErrorMessage = "{0} boş olamaz")]
         public string EmanetGeriAlmaTarih { get; set; }
-
-        [Display(Name = "Emanet İşlem Tarihi")]
         public string EmanetIslemTarih { get; set; }
-        [Display(Name = "Emanet Not")]
         public string EmanetNot { get; set; }
-
-        [Display(Name = "Emanet Teslim Durumu")]
-        [Required(ErrorMessage = "{0} boş olamaz")]
         public string EmanetTeslimEdildi { get; set; }
     }
 }
