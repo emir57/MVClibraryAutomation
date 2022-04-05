824cf45c463a506e27a83cc9c483a4d5189749e3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-05 17:59:24 +0300

    EmanetDto refactoring --> Emanet

--- libraryMVC/Dtos/EmanetDto.cs
+++ libraryMVC/Dtos/EmanetDto.cs
@@ -5,14 +5,8 @@ namespace libraryMVC.Dtos
 {
     public class EmanetDto
     {
-        //TODO: Emanet emanet
-        public int EmanetNo {get; set;}
         public Uye Uye {get; set;}
         public Kitap Kitap {get; set;}
-        public string EmanetVermeTarih {get; set;}
-        public string EmanetGeriAlmaTarih {get; set;}
-        public string EmanetIslemTarih {get; set;}
-        public string EmanetNot {get; set;}
-        public string EmanetTeslimEdildi {get; set;}
+        public EmanetViewModel Emanet { get; set; }
     }
 }
