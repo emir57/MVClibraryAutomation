7e2f8c48a7131fffb30ee4958da7fa054fc801d1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-06 20:45:45 +0300

    add migrations and update database

---
+++ libraryMVC/Migrations/20220406174532_mig5.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace site.Migrations
+{
+    public partial class mig5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UyeNo",
+                table: "Emanetler",
+                newName: "UyeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UyeId",
+                table: "Emanetler",
+                newName: "UyeNo");
+        }
+    }
+}
