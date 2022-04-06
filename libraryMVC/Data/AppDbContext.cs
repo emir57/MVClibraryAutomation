06b911f7d6562fea69ad02f67e31ad3e9098e545
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-06 20:25:12 +0300

    refactoring db context

--- libraryMVC/Data/AppDbContext.cs
+++ libraryMVC/Data/AppDbContext.cs
@@ -1,18 +1,18 @@
 using System.Collections.Generic;
 using libraryMVC.Entities;
 using libraryMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace libraryMVC_.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<Uye>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
         }
         public DbSet<Kitap> Kitaplar{get;set;}
-        public DbSet<Uye> Uyeler{get;set;}
         public DbSet<Emanet> Emanetler{get;set;}
         
     }
