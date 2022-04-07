b6d10c1e6c6098d744b5a70024e1115b749fcb42
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-07 20:17:13 +0300

    _signInManager and _userManager

--- libraryMVC/Controllers/HesapController.cs
+++ libraryMVC/Controllers/HesapController.cs
@@ -1,9 +1,18 @@
+using libraryMVC.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace libraryMVC.Controllers
 {
     public class HesapController : Controller
     {
-        
+        private readonly SignInManager<Uye> _signInManager;
+        private readonly UserManager<Uye> _userManager;
+
+        public HesapController(UserManager<Uye> userManager, SignInManager<Uye> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
     }
 }
