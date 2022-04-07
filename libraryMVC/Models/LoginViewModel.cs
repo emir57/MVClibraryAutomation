c52e2b44a5b602d2c094baadd97b7fb269f15396
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-07 20:25:18 +0300

    add properties and data annotations

--- libraryMVC/Models/LoginViewModel.cs
+++ libraryMVC/Models/LoginViewModel.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace libraryMVC.Models
 {
     public class LoginViewModel
     {
-        
+        [Display(Name = "Eposta")]
+        [Required(ErrorMessage = "Eposta boş olamaz")]
+        public string EPosta { get; set; }
+
+        [Display(Name = "Sifre")]
+        [Required(ErrorMessage = "Şifre boş olamaz")]
+        public string Sifre { get; set; }
     }
 }
