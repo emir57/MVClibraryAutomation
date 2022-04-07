38324427d911a4911224f612ea5e3b641e1ac6c6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-07 16:11:35 +0300

    get context dependency injection

--- libraryMVC/Data/DataSeed/DataSeed.cs
+++ libraryMVC/Data/DataSeed/DataSeed.cs
@@ -1,7 +1,15 @@
+using libraryMVC_.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace libraryMVC.Data.DataSeed
 {
     public class DataSeed
     {
-        
+        public DataSeed(IApplicationBuilder app)
+        {
+            var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetService<AppDbContext>();
+        }
     }
 }
