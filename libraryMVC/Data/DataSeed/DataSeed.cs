2687002507dfae542e4689d68ae7223de297eb1e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-07 16:17:40 +0300

    Data Seed Add Kitaplar

--- libraryMVC/Data/DataSeed/DataSeed.cs
+++ libraryMVC/Data/DataSeed/DataSeed.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using libraryMVC.Entities;
 using libraryMVC_.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +25,42 @@ namespace libraryMVC.Data.DataSeed
             {
 
             }
+        }
+        private static async void AddKitaps(AppDbContext context)
+        {
+            List<Kitap> kitaplar = new List<Kitap>();
+            kitaplar.Add(new Kitap
+            {
+                KitapAd = "Kamelyalı Kadın",
+                KitapYazari = "Alexandre Dumas",
+                KitapBaskiYil = 2019,
+                KitapSayfaSayi = 300,
+                KitapDil = "Türkçe",
+                KitapYayinEvi = "Martı Yayınları",
+                KitapAciklama = "-"
+            });
+            kitaplar.Add(new Kitap
+            {
+                KitapAd = "Nutuk",
+                KitapYazari = "Mustafa Kemal Atatürk",
+                KitapBaskiYil = 1927,
+                KitapSayfaSayi = 351,
+                KitapDil = "Türkçe",
+                KitapYayinEvi = "Panama",
+                KitapAciklama = "-"
+            });
+            kitaplar.Add(new Kitap
+            {
+                KitapAd = "Milena'ya Mektuplar",
+                KitapYazari = "Franz Kafka",
+                KitapBaskiYil = 2019,
+                KitapSayfaSayi = 320,
+                KitapDil = "Türkçe",
+                KitapYayinEvi = "indigo",
+                KitapAciklama = "-"
+            });
+            await context.Kitaplar.AddRangeAsync(kitaplar);
+            await context.SaveChangesAsync();
         }
     }
 }
