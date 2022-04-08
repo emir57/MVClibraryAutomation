363a8380ea720db80ea131c10509823eac2ac95e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-08 18:20:48 +0300

    sign in with password

--- libraryMVC/Controllers/HesapController.cs
+++ libraryMVC/Controllers/HesapController.cs
@@ -1,8 +1,10 @@
 using System.Threading.Tasks;
 using libraryMVC.Entities;
 using libraryMVC.Models;
+using libraryMVC_.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace libraryMVC.Controllers
 {
@@ -10,11 +12,13 @@ namespace libraryMVC.Controllers
     {
         private readonly SignInManager<Uye> _signInManager;
         private readonly UserManager<Uye> _userManager;
+        private readonly AppDbContext _context;
 
-        public HesapController(UserManager<Uye> userManager, SignInManager<Uye> signInManager)
+        public HesapController(UserManager<Uye> userManager, SignInManager<Uye> signInManager, AppDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
         }
         [HttpGet]
         public IActionResult GirisYap()
@@ -23,12 +27,14 @@ namespace libraryMVC.Controllers
             return View(model);
         }
         [HttpPost]
-        public IActionResult GirisYap(LoginViewModel model)
+        public async Task<IActionResult> GirisYap(LoginViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
+            Uye uye = await _context.Users.FirstOrDefaultAsync(x => x.Email == model.EPosta);
+            var result = _signInManager.PasswordSignInAsync(uye, model.Sifre, model.RememberMe, false);
             return RedirectToAction("Kitaplar", "Kitap", new { @message = "Başarıyla Giriş Yapıldı" });
         }
     }
