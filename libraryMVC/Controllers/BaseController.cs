94fe27d6b34fa5c8f68406a607c884ef76e03e62
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-08 19:15:09 +0300

    BaseController

---
+++ libraryMVC/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+using System;
+using libraryMVC_.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using site.Controllers;
+
+namespace libraryMVC.Controllers
+{
+    public class BaseController : Controller
+    {
+        
+    }
+}
