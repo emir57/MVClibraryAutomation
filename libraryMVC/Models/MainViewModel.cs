d9e5365a9f3c0b8d82380c877bbe9aa5ed5d4571
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-08 19:11:49 +0300

    MainViewModel

---
+++ libraryMVC/Models/MainViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using libraryMVC.Entities;
+
+namespace libraryMVC.Models
+{
+    public class MainViewModel
+    {
+        public List<Uye> Uyeler { get; set; }
+    }
+}
