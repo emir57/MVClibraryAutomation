b149d6af69b533db3cec2e58d65a106422fbed95
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-08 19:20:45 +0300

    _Layout get model

--- libraryMVC/Controllers/BaseController.cs
+++ libraryMVC/Controllers/BaseController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using libraryMVC.Models;
 using libraryMVC_.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -8,6 +10,23 @@ namespace libraryMVC.Controllers
 {
     public class BaseController : Controller
     {
-        
+        private readonly AppDbContext _context;
+
+        public BaseController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+            var model = context.Controller.ViewBag.Model as MainViewModel;
+            model
+        }
+
+        protected MainViewModel mainViewModel(){
+            MainViewModel model = new MainViewModel();
+            model.Uyeler = _context.Users.ToList();
+            return model;
+        }
     }
 }
