ce21ac30f7b8a41d8e555cdc4ebaa3da5d7ee18c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-08 17:25:32 +0300

    add migration and database update

---
+++ libraryMVC/Migrations/20220408142503_mig2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace site.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UyeEposta",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Kitaplar",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Kitaplar");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UyeEposta",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
