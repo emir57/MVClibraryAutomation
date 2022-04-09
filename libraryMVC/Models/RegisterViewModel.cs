7bcfa314baa021efc472ece1c41367df0ebc3b46
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-09 18:14:00 +0300

    RegisterViewModel

--- libraryMVC/Models/RegisterViewModel.cs
+++ libraryMVC/Models/RegisterViewModel.cs
@@ -1,7 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace libraryMVC.Models
 {
     public class RegisterViewModel
     {
-        
+        [Display(Name = "Ad")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        public string UyeAd { get; set; }
+        [Display(Name = "Soyad")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        public string UyeSoyad { get; set; }
+        [Display(Name = "Eposta")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        public string Email { get; set; }
+
+        [Display(Name = "Sifre")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        public string Sifre { get; set; }
+
+        [Display(Name = "Sifre Tekrar")]
+        [Required(ErrorMessage = "{0} boş olamaz")]
+        [Compare("Sifre",ErrorMessage = "Şifre eşleşmiyor")]
+        public string SifreTekrar { get; set; }
     }
 }
