662b5745b0aa9f7053b3e16e9b8ba92b2b2c2f6a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-12 18:30:00 +0300

    AdminController DependencyResolvers

--- libraryMVC/Controllers/AdminController.cs
+++ libraryMVC/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using AutoMapper;
+using libraryMVC_.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +9,13 @@ namespace libraryMVC.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        public AdminController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
         public async Task<IActionResult> AdminKitaplar()
         {
             return View();
