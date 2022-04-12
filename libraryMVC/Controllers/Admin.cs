fc54c88f58645450f3a6ebc6a4b5569245da3373
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-12 17:31:59 +0300

    Authorize

--- libraryMVC/Controllers/Admin.cs
+++ libraryMVC/Controllers/Admin.cs
@@ -1,8 +1,10 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace libraryMVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class Admin : Controller
     {
         public async Task<IActionResult> Kitaplar()
